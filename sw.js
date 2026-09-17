2f22098208571d446aa3c93a68e792788409d944
bianosomequipadora-cpu <bianosomequipadora@gmail.com>
2026-09-16 23:39:40 -0300

    fix: keep service worker active for app installation

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim().then(() => self.registration.unregister())));
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache: 'no-store'})); });
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request, {cache: 'no-store'}));
+});
